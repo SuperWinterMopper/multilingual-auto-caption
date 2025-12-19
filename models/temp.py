4a93f142dccfbe7d962c491322efa6527567617e
Keigo Healy <afkhealy@gmail.com>
2025-12-19 20:52:33 +0000

    removed sagemakergit add .!

--- models/temp.py
+++ models/temp.py
@@ -1,7 +1,8 @@
-from sagemaker.serve.model_builder import ModelBuilder
+import numpy as np
 
 def main():
     print("hello world")
+    print(np.array([1,2,3]))
 
 if __name__ == '__main__':
     main()
